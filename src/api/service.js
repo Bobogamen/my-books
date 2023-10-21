6f53ce7cdbccb32a9741626ebcf4c8634891c82c
Bobogamen <b_davidov@abv.bg>
2023-10-21 15:04:48 +0300

    add Dockerfile for deploy

--- src/api/service.js
+++ src/api/service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const host = 'http://localhost:8080'
+const host = 'http://mybooks-server.railway.internal'
 
 export function getAllBooks() {
     return axios.get(`${host}/books`)
