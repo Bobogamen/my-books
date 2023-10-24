13ea528ea138696d1d4db161542a1fdafe804553
Bobogamen <b_davidov@abv.bg>
2023-10-25 00:29:13 +0300

    add sort by title and author

--- src/component/ListAllBooks.jsx
+++ src/component/ListAllBooks.jsx
@@ -1,36 +1,83 @@
 import React, { useEffect, useState } from 'react';
 import { getAllBooks } from '../api/service';
+import { BsSortAlphaDown, BsSortAlphaDownAlt } from "react-icons/bs";
 
 function ListAllBooks() {
     const [books, setBooks] = useState([]);
+    const [sortedBooks, setSortedBooks] = useState([]);
+    const [sortByTitle, setSortByTitle] = useState(false);
+    const [sortByAuthor, setSortByAuthor] = useState(false);
 
     useEffect(() => {
-        getAllBooks().then(response => {
-            setBooks(response.data)
-        })
-            .then(res => res.json)
-            .catch(res => res.json)
-    }, [books]);
+        const fetchData = async () => {
+            try {
+                const response = await getAllBooks();
+                const data = response.data;
+                setBooks(data);
+                setSortedBooks([...data]);
+            } catch (error) {
+                console.error('Error fetching books:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleSortByTitle = () => {
+        if (sortByTitle) {
+            setSortedBooks([...books].sort((a, b) => b.title.localeCompare(a.title)))
+        } else {
+            setSortedBooks([...books].sort((a, b) => a.title.localeCompare(b.title)))
+        }
+        setSortByTitle(!sortByTitle);
+    };
+
+    const handleSortByAuthor = () => {
+        if (sortByAuthor) {
+            setSortedBooks([...books].sort((a, b) => b.author.name.localeCompare(a.author.name)))
+        } else {
+            setSortedBooks([...books].sort((a, b) => a.author.name.localeCompare(b.author.name)))
+        }
+        setSortByAuthor(!sortByAuthor);
+    };
 
     return (
         <div>
             <div className="d-flex justify-content-center my-2">
-                <h2 className="text-center fw-bold mx-2 m-auto">Книги {books.length}бр.</h2>
+                <h2 className="text-center fw-bold mx-2 m-auto">Книги {books.length} бр.</h2>
                 <a href="/add-book">
                     <button className="btn btn-success fw-bold">Добави</button>
                 </a>
             </div>
             <div className="container">
-                {books.length > 0 ?
+                {sortedBooks.length > 0 ?
                     <table className="table table-sm table-bordered text-center align-middle">
                         <thead className="h6">
                             <tr>
-                                <th>Заглавие</th>
-                                <th>Автор</th>
+                                <th>
+                                    <div className="h4 p-0 m-0">
+                                        <span className="mx-1 fw-bold">Заглавие</span>
+                                        { sortByTitle ?
+                                            <BsSortAlphaDown className="border border-black rounded h3" onClick={handleSortByTitle}/>
+                                            :
+                                            <BsSortAlphaDownAlt className="border border-black rounded h3" onClick={handleSortByTitle}/>
+                                        }
+                                    </div>
+                                </th>
+                                <th>
+                                    <div className="h4 p-0 m-0">
+                                        <span className="mx-1 fw-bold">Автор</span>
+                                        { sortByAuthor ?
+                                            <BsSortAlphaDown className="border border-black rounded h3" onClick={handleSortByAuthor}/>
+                                            :
+                                            <BsSortAlphaDownAlt className="border border-black rounded h3" onClick={handleSortByAuthor}/>
+                                        }
+                                    </div>
+                                </th>
                             </tr>
                         </thead>
                         <tbody className="h6">
-                            {books.map(b => (
+                            {sortedBooks.map(b => (
                                 <tr key={b.id}>
                                     <td>
                                         <a href={`/book/${b.id}`}>{b.title}</a>
