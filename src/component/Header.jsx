fc586db48aa3ea745f9c253cfdff81e590c5ce0c
Bobogamen <b_davidov@abv.bg>
2023-12-14 05:24:44 +0200

    fix for build

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SearchBar from "./Search";
 import eng from "../assets/images/eng.png"
 import bg from "../assets/images/bg.png"
